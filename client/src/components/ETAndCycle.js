523c248f81dfbcdf24d28cdc92f8acb5fc2aba52
Chase <chasedemaster.tech@gmail.com>
2022-09-15 12:32:40 -0500

    closer to getting user session's month's ET

--- client/src/components/ETAndCycle.js
+++ client/src/components/ETAndCycle.js
@@ -19,23 +19,28 @@ import rotorIcon from "./i-1.png";
 import sprayIcon from "./j-3.png";
 // import sprayDark from "./j-4.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee, faCalendar, faDroplet, faDropletSlash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCoffee,
+  faCalendar,
+  faDroplet,
+  faDropletSlash,
+} from "@fortawesome/free-solid-svg-icons";
 
-function ETAndCycle({user, sessionMonths}) { 
-console.log(sessionMonths)
-
-  const calendar = <FontAwesomeIcon icon={faCalendar} />
-  const waterIcon = <FontAwesomeIcon icon={faDroplet} />
-  const waterSlashIcon = <FontAwesomeIcon icon={faDropletSlash} />
-  const element = <FontAwesomeIcon icon={faCoffee} />
-  const seasonalLink = "https://www.youtube.com/watch?v=IjwJFnB6wj4"
+function ETAndCycle({ user, sessionMonths }) {
+  const calendar = <FontAwesomeIcon icon={faCalendar} />;
+  const waterIcon = <FontAwesomeIcon icon={faDroplet} />;
+  const waterSlashIcon = <FontAwesomeIcon icon={faDropletSlash} />;
+  const element = <FontAwesomeIcon icon={faCoffee} />;
+  const seasonalLink = "https://www.youtube.com/watch?v=IjwJFnB6wj4";
   const APIET = "www.google.com";
   const APIKey =
     "OTc0MDk4N2VjN2FhNDZhMTkyYzhiMDFmNWIzMDlhNDU6ODVjYWM1ZWMtMWVhNi00NjdhLTkzZGYtMzdlZjdmYzNkYmYx";
-
-  const [monthETState, setMonthETState] = useState();
-  // const [ET, setET] = useState(.9)
-  const [seasonal, setSeasonal] = useState(null)
+  console.log(sessionMonths);
+  console.log(sessionMonths);
+  // console.log(month);
+  // console.log(sessionMonths[month]);
+  const [monthETState, setMonthETState] = useState([]);
+  const [seasonal, setSeasonal] = useState(null);
 
   let monthET = monthETState; //inches
   let cropET = (monthET * 0.65).toFixed(2);
@@ -44,17 +49,15 @@ console.log(sessionMonths)
   let rotor = (((weekET / 0.625) * 60) / cyclesPerWeek).toFixed(0);
   let spray = (((weekET / 1.5) * 60) / cyclesPerWeek).toFixed(0);
 
-  // console.log(seasonal)
-
-  useEffect(getSeasonal, [])
-  function getSeasonal () {
-    let big = ((Object.entries(ETPerMonthInHouston))[6][1])
+  useEffect(getSeasonal, []);
+  function getSeasonal() {
+    let big = Object.entries(ETPerMonthInHouston)[6][1];
     // console.log(big)
-    let small = thisMonthsET[0][1]
-    let seasonalMathLong = ((small)/(big)*100)
+    let small = thisMonthsET[0][1];
+    let seasonalMathLong = (small / big) * 100;
     // console.log(seasonalMathLong)
-    setSeasonal(roundNearest5(seasonalMathLong))
-    function roundNearest5(num) { 
+    setSeasonal(roundNearest5(seasonalMathLong));
+    function roundNearest5(num) {
       return Math.round(num / 5) * 5;
     }
   }
@@ -72,12 +75,6 @@ console.log(sessionMonths)
   }
   useEffect(setET, []);
 
-  
-
-  // function Math () {
-  // {ET}
-  // }
-
   const monthsAsStrings = [
     "January",
     "February",
@@ -94,10 +91,20 @@ console.log(sessionMonths)
   ];
   const d = new Date();
   let month = monthsAsStrings[d.getMonth()];
-  // const setMonth = () => {
-  //   setMonthState(month)
+
+  // useEffect(() => {
+  //   usersCitysETBasedOnWhatMonthItCurrentlyIs()
+  //     }, [month])
+
+  // function usersCitysETBasedOnWhatMonthItCurrentlyIs () {
+  //   const thisCitysMonth = sessionMonths.find(
+  //     (m) => m === month
+  //   );
+  //   console.log(thisCitysMonth)
+  //   monthETState(thisCitysMonth.value)
   // }
 
+  // hard code for dynamic to month but not dynamic to user, ET reads
   const ETPerMonthInHouston = {
     January: 1.0,
     February: 1.5,
@@ -107,14 +114,14 @@ console.log(sessionMonths)
     June: 7.8,
     July: 8.2,
     August: 7.8,
-    September: 6.0,
+    September: 1.0,
     October: 4.5,
     November: 2.2,
     December: 1.3,
-  }
+  };
 
   const ETsAsArray = Object.entries(ETPerMonthInHouston);
-  
+
   const filter = ETsAsArray.filter(([key, value]) => typeof value === "string");
   // console.log(filter)
 
@@ -127,8 +134,6 @@ console.log(sessionMonths)
   // console.log((thisMonthsET[0][1]));
 
   //seasonal math
-  
-  
 
   const bull = (
     <Box
@@ -139,10 +144,11 @@ console.log(sessionMonths)
     </Box>
   );
   // console.log(rotor, rotorDark, spray, sprayDark)
+
+  console.log(user.city.name);
+
   return (
-    
     <div className="historical">
-      
       <Box
         m={5}
         pt={5}
@@ -161,16 +167,14 @@ console.log(sessionMonths)
               color="text.secondary"
               gutterBottom
             >
-              {calendar}  {month}
-
+              {calendar} {month}
             </Typography>
             <Typography variant="h5" component="div" sx={{ mb: 1.5 }}>
-              Houston               (get user city)
-
+              {user.city.name}
             </Typography>
             <br></br>
             <Box align="center">
-              <Typography variant="body" sx={{ mb: 1 }} >
+              <Typography variant="body" sx={{ mb: 1 }}>
                 <img src={rotorIcon} style={{ width: "auto", height: 105 }} />{" "}
                 Set rotor zones to {rotor} min per cycle
                 <p></p>
@@ -179,11 +183,10 @@ console.log(sessionMonths)
               </Typography>
             </Box>
             <Box align="center">
-            <Typography m={3} sx={{ mb: 2 }} color="text.secondary">
-              {/* Your Schedule: */}
-              <br></br>
-
-            </Typography>
+              <Typography m={3} sx={{ mb: 2 }} color="text.secondary">
+                {/* Your Schedule: */}
+                <br></br>
+              </Typography>
             </Box>
             <Typography>
               <Stack
@@ -192,14 +195,13 @@ console.log(sessionMonths)
                 divider={<Divider orientation="horizontal" flexItem />}
                 spacing={2}
               >
-                 <Item>Monday: {waterIcon}</Item>
+                <Item>Monday: {waterIcon}</Item>
                 <Item>Tuesday: {waterSlashIcon}</Item>
                 <Item>Wednesday: {waterIcon}</Item>
                 <Item>Thursday: {waterSlashIcon}</Item>
                 <Item>Friday: {waterIcon}</Item>
                 <Item>Saturday: {waterSlashIcon}</Item>
                 <Item>Sunday:{waterIcon}</Item>
-
               </Stack>
               <Box align="center">
                 <Typography
@@ -208,10 +210,12 @@ console.log(sessionMonths)
                   sx={{ mb: 1 }}
                   color="text.secondary"
                 >
-                
-                  * or <a href={seasonalLink}>seasonally adjust</a> to {seasonal
-                  //  !== null ? {getSeasonal} : "75"
-                   }%
+                  * or <a href={seasonalLink}>seasonally adjust</a> to{" "}
+                  {
+                    seasonal
+                    //  !== null ? {getSeasonal} : "75"
+                  }
+                  %
                 </Typography>
               </Box>
             </Typography>
@@ -220,7 +224,6 @@ console.log(sessionMonths)
             {/* <Button size="small">Learn More</Button> */}
           </CardActions>
         </Card>
-       
 
         {/* <Schedule /> */}
       </Box>
