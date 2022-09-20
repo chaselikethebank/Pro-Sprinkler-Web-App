b8c0699b5fc8005f794e12886e78b2eadf66bce4
Chase <chasedemaster.tech@gmail.com>
2022-09-20 10:01:39 -0500

    muted all the long lat fetches (saved for future)

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,8 @@
 import "./App.css";
-// import Main from "./components/Nav";
-// import Location from "./components/Location";
 import Forecast from "./components/Forecast";
 import { useState, useEffect } from "react";
-// import { Routes, Route } from 'react-router-dom';
-// import Login from "./components/Login";
-// import Nav from "./components/Nav";
 import Signup from "./components/Signup";
-// import Schedule from "./components/Schedule";
 import ETAndCycle from "./components/ETAndCycle";
-// import Time from "./components/Time";
 import About from "./components/About";
 import { ListItem } from "@mui/material";
 import NewNav from "./components/NewNav";
@@ -42,43 +35,43 @@ function App() {
   // console.log(lat)
   // console.log(lon)
 
-  function getCord() {
-    fetch(
-      `https://api.myptv.com/geocoding/v1/locations/by-address?locality=${local[0]}&postalCode=77345`,
-      {
-        method: "GET",
-        headers: {
-          apiKey:
-            "OTc0MDk4N2VjN2FhNDZhMTkyYzhiMDFmNWIzMDlhNDU6ODVjYWM1ZWMtMWVhNi00NjdhLTkzZGYtMzdlZjdmYzNkYmYx",
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((result) => setLatLon(result.locations[0].referencePosition));
-  }
-  useEffect(getCord, []);
+  // function getCord() {
+  //   fetch(
+  //     `https://api.myptv.com/geocoding/v1/locations/by-address?locality=${local[0]}&postalCode=77345`,
+  //     {
+  //       method: "GET",
+  //       headers: {
+  //         apiKey:
+  //           "OTc0MDk4N2VjN2FhNDZhMTkyYzhiMDFmNWIzMDlhNDU6ODVjYWM1ZWMtMWVhNi00NjdhLTkzZGYtMzdlZjdmYzNkYmYx",
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   )
+  //     .then((response) => response.json())
+  //     .then((result) => setLatLon(result.locations[0].referencePosition));
+  // }
+  // useEffect(getCord, []);
   // console.log(latLon);
 
-  function getForecast() {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=29&lon=-95&appid=${APIKeyForecast}&units=imperial`
-    )
-      .then((res) => res.json())
-      .then((data) => setForecast(data.weather));
-  }
-  useEffect(getForecast, []);
+  // function getForecast() {
+  //   fetch(
+  //     `https://api.openweathermap.org/data/2.5/weather?lat=29&lon=-95&appid=${APIKeyForecast}&units=imperial`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => setForecast(data.weather));
+  // }
+  // useEffect(getForecast, []);
   // console.log(forecast)
 
-  function getTemp() {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=29.64&lon=-95.28&appid=${APIKeyForecast}&units=imperial`
-    )
-      .then((res) => res.json())
-      .then((data) => setTemp(data));
-  }
-  useEffect(getTemp, []);
-  // console.log(temp)
+  // function getTemp() {
+  //   fetch(
+  //     `https://api.openweathermap.org/data/2.5/weather?lat=29.64&lon=-95.28&appid=${APIKeyForecast}&units=imperial`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => setTemp(data));
+  // }
+  // useEffect(getTemp, []);
+  // // console.log(temp)
 
   const renderForecast = forecast.map((weather) => (
     <Forecast local={local} key={forecast.id} forecast={forecast} temp={temp} />
@@ -91,21 +84,10 @@ function App() {
   }
   useEffect(getCet, []);
 
-  // useEffect(findCity, []);
 
   const [sessionMonths, setSessionMonths] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("/me").then((r) => {
-  //     if (r.ok) {
-  //       debugger;
-  //       r.json()
-  //         // .then((user) => setUser(user))
-  //         // .then(() => findCity())
-  //         .then(() => setUser(user));
-  //     }
-  //   });
-  // }, []);
+
   
 
   useEffect(() => {
